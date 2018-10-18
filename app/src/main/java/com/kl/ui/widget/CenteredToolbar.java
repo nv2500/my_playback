bd34a3222e83d9c999d6bb75184f78f0ebd0b7cb
Nhat Vo <vothanhnhat85@gmail.com>
2018-10-18 11:25:39 -0300

    update DarkUI mode

---
+++ app/src/main/java/com/kl/ui/widget/CenteredToolbar.java
@@ -0,0 +1,76 @@
+package com.kl.ui.widget;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
+
+public class CenteredToolbar extends Toolbar {
+
+    private TextView centeredTitleTextView;
+
+    public CenteredToolbar(Context context) {
+        super(context);
+    }
+
+    public CenteredToolbar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CenteredToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setTitle(@StringRes int resId) {
+        String s = getResources().getString(resId);
+        setTitle(s);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        getCenteredTitleTextView().setText(title);
+    }
+
+    @Override
+    public CharSequence getTitle() {
+        return getCenteredTitleTextView().getText().toString();
+    }
+
+    public void setTypeface(Typeface font) {
+        getCenteredTitleTextView().setTypeface(font);
+    }
+
+    private TextView getCenteredTitleTextView() {
+        if (centeredTitleTextView == null) {
+            final Context context = getContext();
+
+            centeredTitleTextView = new TextView(context);
+
+            centeredTitleTextView.setTextAppearance(getContext(), R.style.mAppbarTitle);
+
+            Typeface fontTypeface = ResourcesCompat.getFont(context, R.font.montserrat_medium);
+            centeredTitleTextView.setTypeface(fontTypeface);
+
+            centeredTitleTextView.setSingleLine();
+            centeredTitleTextView.setEllipsize(TextUtils.TruncateAt.END);
+            centeredTitleTextView.setGravity(Gravity.CENTER);
+
+            Toolbar.LayoutParams lp = new Toolbar.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            lp.gravity = Gravity.CENTER;
+            centeredTitleTextView.setLayoutParams(lp);
+
+            addView(centeredTitleTextView);
+        }
+        return centeredTitleTextView;
+    }
+}
